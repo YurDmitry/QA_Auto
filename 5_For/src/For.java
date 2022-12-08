72c66bfbef044722be5bd4619d0d7a08eaba7129
Dmitry Yurchenko <dzmitry_yurchanka@senla.eu>
2022-12-08 11:26:07 +0300

    5_For

---
+++ 5_For/src/For.java
@@ -0,0 +1,14 @@
+public class For {
+	
+	public static void main(String[] strg) {
+		System.out.print("Drawing a figure from asterisks:\n");
+		for(int i = 0; i < 3; i++) {
+			System.out.print("\t\t");
+			for(int n = 0; n < 5; n++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+	}
+}
