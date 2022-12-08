6273c3e697cd7e2cafb05f0aa69c2c217145a7b6
Dmitry Yurchenko <dzmitry_yurchanka@senla.eu>
2022-12-08 10:29:40 +0300

    2_Variables

---
+++ 2_Variables/src/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+	public static void main(String[] strg) {
+		int a = (int)(Math.random()*10 + 1);
+		double b = a * 0.5;
+		char c = '.';
+		String str = "Multiply a*0.5\nWe get: ";
+		System.out.println(str + b + c);
+	}
+}
